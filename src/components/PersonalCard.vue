21cd7f010b20311689104aa7bc22d0ae44630d3d
Gerald Ramos <ramosgerald255@gmail.com>
2022-02-16 17:50:21 +0800

    latest push

--- src/components/PersonalCard.vue
+++ src/components/PersonalCard.vue
@@ -2,8 +2,8 @@
   <div>
     <h3 class="title uppercase text-lg font-semibold">Gerald Ramos</h3>
     <br>
-    <div class="data mb-2.5 text-sm"><strong>E-mail:</strong> ramosgerald255@gmail.com</div>
-    <div class="data mb-2.5 text-sm"><strong>Phone:</strong> +63 955 802 0250</div>
+    <div class="data mb-2.5 text-sm"><strong>E-mail:</strong> lemcatipay@gmail.com</div>
+    <div class="data mb-2.5 text-sm"><strong>Phone:</strong> +63 955 802 6969</div>
     <div class="data mb-2.5 text-sm"><strong>City:</strong> Tarlac, Philippines</div>
     <div class="data mb-2.5 text-sm"><strong>Languages:</strong> Tagalog, English</div>
     <SocialBar/>
